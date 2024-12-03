b50232ddc81492b3dd6846afa936c4ccf0d5600a
ysj0701 <syj0701@surff.kr>
2024-12-03 18:11:40 +0900

    feat: game controller 1st

---
+++ src/main/java/chess/controller/GameController.java
@@ -0,0 +1,32 @@
+package chess.controller;
+
+import org.apache.tomcat.util.http.ResponseUtil;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+
+    private final ChessService chessService;
+
+    @PostMapping("/create")
+    public CreateGameDto createGame() {
+        return chessService.createGame();
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView findGame(@PathVariable int id) {
+        GameDto gameDto = chessService.findGame(id);
+
+        return ResponseUtil.createModelAndView(HTML_TEMPLATE_PATH, gameDto);
+    }
+
+    @PostMapping("/{id}")
+    public ModelAndView playGame(@PathVariable int id,
+                                 @RequestBody MoveRoute moveRoute) {
+        chessService.playGame(id, new MoveEvent(moveRoute));
+        GameDto gameDto = chessService.findGame(id);
+        return ResponseUtil.createModelAndView(HTML_TEMPLATE_PATH, gameDto);
+    }
+}
